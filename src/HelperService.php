bced4553d5bb6fef563b1a800a0167e9730765bf
zghua <1803897171@qq.com>
2020-09-14 14:34:15 +0800

    init

---
+++ src/HelperService.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: admin
+	 * Date: 2020/9/14
+	 * Time: 14:12
+	 */
+
+	namespace guohua_php;
+
+
+	class HelperService
+	{
+		public function test(){
+			echo "this is a test";
+		}
+	}
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: admin
+	 * Date: 2020/9/14
+	 * Time: 14:14
+	 */
+	require_once __DIR__ . "/vendor/autoload.php";
+	use guohua_php\HelperService;
+
+	$class = new HelperService();
+	$class->test();
+
+
