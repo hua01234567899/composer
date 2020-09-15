950f0f9f36041df406f858e69a7f2375fede0892
zghua <1803897171@qq.com>
2020-09-15 08:49:46 +0800

    增加service/Helper

---
+++ src/Service/Http.php
@@ -0,0 +1,53 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: admin
+	 * Date: 2020/9/15
+	 * Time: 8:26
+	 */
+
+	namespace guohua_php\Service;
+
+
+	class Http
+	{
+
+		public static function curlGet($url = '', $options = array())
+		{
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+			if (!empty($options)) {
+				curl_setopt_array($ch, $options);
+			}
+			//https请求 不验证证书和host
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			$data = curl_exec($ch);
+			curl_close($ch);
+			return $data;
+		}
+
+
+		public static function curlPost($url = '', $postData = '', $options = array())
+		{
+			if (is_array($postData)) {
+				$postData = http_build_query($postData);
+			}
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 30); //设置cURL允许执行的最长秒数
+			if (!empty($options)) {
+				curl_setopt_array($ch, $options);
+			}
+			//https请求 不验证证书和host
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			$data = curl_exec($ch);
+			curl_close($ch);
+			return $data;
+		}
+	}
